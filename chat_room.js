0f4eb72a1dc084c5102c4ea67342f867ffd9f44f
Aarushg28 <85989480+Aarushg28@users.noreply.github.com>
2022-02-08 20:06:25 +0530

    Add files via upload

---
+++ chat_room.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAJbeoWuFdQu6qhKRUibmqxq1av21gB3es",
+    authDomain: "letschat-8c349.firebaseapp.com",
+    projectId: "letschat-8c349",
+    storageBucket: "letschat-8c349.appspot.com",
+    messagingSenderId: "501953843322",
+    appId: "1:501953843322:web:155bff9b8846f0520debc3"
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
